4c61d8fe8c309acc519542b3ca9217f2f0b73a42
TheAbyssWalker <superrap4761@gmail.com>
2018-01-20 19:16:13 +0530

    Functions for detectinig peaks and valleys

--- test.py
+++ test.py
@@ -1,58 +1,107 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 26 17:18:18 2017
-@author: Rahul
-"""
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-def Coordinates(edges):
-    wid_pix=list()
-    leng_pix=list()
-    for columns in range(edges.shape[1]):
-        for rows in range(edges.shape[0]):
-            if(edges[rows][columns]!=0):
-                wid_pix.append(rows)
-                leng_pix.append(columns)
-                break
-    return wid_pix
 
 
+def find_peak(pos):
+    rows = pos.shape[0]
+    print(rows)
+    check_width = 20
+    p_row = np.array([], np.int16)
+    p_column = np.array([], np.int16)
+    i = check_width
+    while i < rows - check_width:
+        if pos[i] <= pos[i-1] and pos[i] <= pos[i+1]:
+            peak_flag = True
+            for p in range(1, check_width):
+                if pos[i] > pos[i-p] or pos[i] > pos[i+p]:
+                    peak_flag = False
+                    break
+
+            if peak_flag:
+                p_row = np.append(p_row, pos[i])
+                p_column = np.append(p_column, i+5)
+                i += check_width - 5
+        i += 1
+    print(p_row.shape)
+    return p_row, p_column
 
 
+def find_valley(pos):
+    rows = pos.shape[0]
+    print(rows)
+    check_width = 20
+    v_row = np.array([], np.int16)
+    v_column = np.array([], np.int16)
+    i = check_width
+    while i < rows - check_width:
+        if pos[i] >= pos[i-1] and pos[i] >= pos[i+1]:
+            valley_flag = True
+            for p in range(1, check_width):
+                if pos[i] < pos[i-p] or pos[i] < pos[i+p]:
+                    valley_flag = False
+                    break
+
+            if valley_flag:
+                v_row = np.append(v_row, pos[i])
+                v_column = np.append(v_column, i+5)
+                i += check_width - 5
+        i += 1
+    print(v_row.shape)
+    return v_row, v_column
 
 
-image=cv2.imread('znap24.jpg',cv2.IMREAD_COLOR)
-#img2gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-red = image[400:600,500:1700,2]
-red1=cv2.bilateralFilter(red,10,100,100)
-ret,new=cv2.threshold(red,80,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+def show_peak_valley(img, v_row, v_column, p_row, p_column):
+    #cv2.circle(img, (250, 250), 50, (100, 50, 200), -1)
+    for i in range(v_row.shape[0]):
+        cv2.circle(img, (v_column[i], int(v_row[i])), 1, (0, 255, 0), -1)
 
-ret1,new1=cv2.threshold(red1,80,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-cv2.imshow('res1',new)
-#cv2.imshow('res',new1)
-'''kernel = np.ones((5,5),np.uint8)
+    for i in range(p_row.shape[0]):
+        cv2.circle(img, (p_column[i], int(p_row[i])), 1, (255, 0, 0), -1)
 
-ero = cv2.erode(new, kernel, iterations=1)
-dil = cv2.dilate(ero, kernel,iterations=1)
-dil = cv2.dilate(dil, kernel,iterations=1)
-ero = cv2.erode(dil, kernel, iterations=1)
-cv2.imshow('wq',ero)'''
-edges=cv2.Canny(new1,100,200)
-edges1=cv2.Canny(new,100,200)
-wid_pixel=Coordinates(edges)
-wid_pixel1=Coordinates(edges1)
+    cv2.imshow("valleysssss", img)
 
-cv2.imshow('edges',edges)     
-viz=pd.Series(wid_pixel)
-viz1=pd.Series(wid_pixel1)
-plt.plot(viz)
-plt.plot(viz1)
-plt.show()
-#length=leng_pix*sizeofeachpixel
+img = cv2.imread('/home/theabysswalker/Documents/python_files/znap24.jpg')
 
+img = img[400:600, 600:1200]
+
+redChannel = img[:, :, 2]
+
+#redChannel = cv2.bilateralFilter(redChannel, 10, 100, 100)
+
+retval,binaryImage = cv2.threshold(redChannel, 80, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+'''
+kernel = np.array([[0,0,1],[0,1,1],[1,1,1]],np.uint8)
+kernel = np.ones((15, 15), np.uint8)
+binaryImage2 = cv2.morphologyEx(binaryImage, cv2.MORPH_CLOSE, kernel)
+'''
+
+thread_row = np.array([])
+
+for column in range(1, binaryImage.shape[1]):
+
+    for row in range(binaryImage.shape[0]):
+        if binaryImage[row, column] != 0:
+            thread_row = np.append(thread_row, row)
+            break
+
+valley_row, valley_column = find_valley(thread_row)
+
+peak_row, peak_column = find_peak(thread_row)
+
+print("Peaks:")
+print(peak_row)
+print("their pos:")
+print(peak_column)
+
+print("Valleys")
+print(valley_row)
+print("their pos:")
+print(valley_column)
+
+show_peak_valley(img, valley_row, valley_column, peak_row, peak_column)
+
+#cv2.imshow('org', redChannel)
+cv2.imshow('otsu', binaryImage)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
