39272dc464023866a68791768ad24396fc69e123
TheAbyssWalker <superrap4761@gmail.com>
2018-02-24 16:20:48 +0530

    stop at the end of thread

--- bolt_measure.py
+++ bolt_measure.py
@@ -1,62 +1,89 @@
 import cv2
+import time
 import numpy as np
 
 
-def find_peak(pos):
-    rows = pos.shape[0]
-    print(rows)
+def find_peak(th_row, th_col):
+    
+    rows = th_row.shape[0]
     check_width = 20
     p_row = np.array([], np.int16)
     p_column = np.array([], np.int16)
     i = check_width
+    end_flag = False
+    first_time = True
+
     while i < rows - check_width:
-        if pos[i] <= pos[i-1] and pos[i] <= pos[i+1]:
+        
+        if th_row[i] <= th_row[i-1] and th_row[i] <= th_row[i+1]:
             peak_flag = True
+            
             for p in range(1, check_width):
-                if pos[i] > pos[i-p] or pos[i] > pos[i+p]:
+                if th_row[i] > th_row[i-p] or th_row[i] > th_row[i+p]:
                     peak_flag = False
                     break
 
             if peak_flag:
-                p_row = np.append(p_row, pos[i])
-                p_column = np.append(p_column, i+50)
+                if first_time:
+                    first_time = False
+                elif p_row[-1] - th_row[i] > 50:
+                    end_flag = True
+                    break
+                p_row = np.append(p_row, th_row[i])
+                p_column = np.append(p_column, th_col[i])
                 i += check_width - 5
+        
+        if end_flag:
+            break
         i += 1
-    print(p_row.shape)
+    
     return p_row, p_column
 
 
-def find_valley(pos):
-    rows = pos.shape[0]
-    print(rows)
-    check_width = 20
+def find_valley(th_row, th_col):
+    
+    rows = th_row.shape[0]
+    check_width = 30
     v_row = np.array([], np.int16)
     v_column = np.array([], np.int16)
     i = check_width
+    end_flag = False
+    first_time = True
+
     while i < rows - check_width:
-        if pos[i] >= pos[i-1] and pos[i] >= pos[i+1]:
+        
+        if th_row[i] >= th_row[i-1] and th_row[i] >= th_row[i+1]:
             valley_flag = True
+            
             for p in range(1, check_width):
-                if pos[i] < pos[i-p] or pos[i] < pos[i+p]:
+                if th_row[i] < th_row[i-p] or th_row[i] < th_row[i+p]:
                     valley_flag = False
                     break
 
             if valley_flag:
-                v_row = np.append(v_row, pos[i])
-                v_column = np.append(v_column, i+50)
+                if first_time:
+                    first_time = False
+                elif (v_row[-1] - th_row[i]) > 50:
+                        end_flag = True
+                        break
+                v_row = np.append(v_row, th_row[i])
+                v_column = np.append(v_column, th_col[i])
                 i += check_width - 5
+        
+        if end_flag:
+            break
         i += 1
-    print(v_row.shape)
+    
     return v_row, v_column
 
 
 def show_peak_valley(img, v_row, v_column, p_row, p_column):
     #cv2.circle(img, (250, 250), 50, (100, 50, 200), -1)
     for i in range(v_row.shape[0]):
-        cv2.circle(img, (v_column[i], int(v_row[i])), 1, (0, 255, 0), -1)
+        cv2.circle(img, (int(v_column[i]), int(v_row[i])), 1, (0, 255, 0), -1)
 
     for i in range(p_row.shape[0]):
-        cv2.circle(img, (p_column[i], int(p_row[i])), 1, (255, 0, 0), -1)
+        cv2.circle(img, (int(p_column[i]), int(p_row[i])), 1, (255, 0, 0), -1)
 
     cv2.imshow("valleysssss", img)
 
@@ -64,25 +91,21 @@ def show_peak_valley(img, v_row, v_column, p_row, p_column):
 
 img = cv2.imread('/home/theabysswalker/Documents/python_files/znap24.jpg')
 
-img = img[400:600, 500:1700]
+img = img[400:600, 500:1800]
 
 redChannel = img[:, :, 2]
 
 #redChannel = cv2.bilateralFilter(redChannel, 10, 100, 100)
 
 retval,binaryImage = cv2.threshold(redChannel, 80, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-'''
-kernel = np.array([[0,0,1],[0,1,1],[1,1,1]],np.uint8)
-kernel = np.ones((15, 15), np.uint8)
-binaryImage2 = cv2.morphologyEx(binaryImage, cv2.MORPH_CLOSE, kernel)
-'''
 
 thread_row = np.array([])
-
+thread_col = np.array([])
 top=0
 bottom=0
 #print(binaryImage.shape[0])
 #print(binaryImage.shape[1])
+'''start = time.time()
 
 for column in range(1, binaryImage.shape[1]):
     top_flag = False
@@ -103,30 +126,46 @@ for column in range(binaryImage.shape[1] - 1, top, -1):
 
     if bottom != 0:
         break
-print(top)
-print(bottom)
+stop = time.time()
 '''
-for column in range(top, bottom):
+for column in range(binaryImage.shape[1]):
 
     for row in range(binaryImage.shape[0]):
         if binaryImage[row, column] != 0:
             thread_row = np.append(thread_row, row)
+            thread_col = np.append(thread_col, column)
             break
 
-valley_row, valley_column = find_valley(thread_row)
+temp = np.diff(thread_row, 1)
+temp = np.where(temp < -50)
+print("Hello")
+print(temp)
 
-peak_row, peak_column = find_peak(thread_row)
+
+valley_row, valley_column = find_valley(thread_row, thread_col)
+
+peak_row, peak_column = find_peak(thread_row, thread_col)
+
+top = thread_col[0]
+bottom = thread_col[-1]
+
+print("No of pixels from top to bottom is {}".format((bottom - top)*5.9/1104))
+#print("Time is {}".format(stop-start))
+
 
 print("Peaks:")
 print(peak_row)
 print("their pos:")
 print(peak_column)
+print("peak diff")
+print(np.diff(peak_column, 1))
 
 print("Valleys")
 print(valley_row)
 print("their pos:")
 print(valley_column)
-
+print("valley diff")
+print(np.diff(valley_column,1)*5.9/1104)
 show_peak_valley(img, valley_row, valley_column, peak_row, peak_column)
 
 #cv2.imshow('org', redChannel)
@@ -134,4 +173,3 @@ cv2.imshow('otsu', binaryImage)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
